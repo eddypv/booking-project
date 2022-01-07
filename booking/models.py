85be6b01d2881b6bd666880e829052e7df000011
Eddy <eddypv0@gmail.com>
2022-01-06 23:08:04 -0400

    New models Room and RoomFacility

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,21 @@
 from django.db import models
+from django.db.models.base import Model
 
 class RoomService(models.Model):
-  cod_service = models.CharField(max_length=20,primary_key=True)
+  cod_service = models.CharField(max_length=20, primary_key=True)
   service = models.CharField(max_length=100)
+
+class Room(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.CharField(max_length=1000)
+  guests = models.PositiveSmallIntegerField() # cantidad huspedes
+  beds = models.PositiveSmallIntegerField() #cantidad camas
+  toilets = models.PositiveSmallIntegerField() #cantidad camas
+
+class RoomFacility(models.Model):
+    cod_facility = models.CharField(max_length=20, primary_key=True)
+    facility = models.CharField(max_length=100)
+    
+
+
+
