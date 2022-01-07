70968faa3d8eb22768da2b2be0868bcc23b9510e
Eddy Perez <eddypv0@gmail.com>
2022-01-07 11:25:45 -0400

    Create view search_rooms

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,8 @@
-from booking.views import hello_word
+from booking.views import hello_word, search_rooms
 from django.urls import path 
 
 urls =[
-    path('api/hello-word', hello_word, name="hello-word")
+    path('api/hello-word', hello_word, name="hello-word"),
+    path('api/booking/search_rooms/<str:start_date>/<str:end_date>/<int:guests>', search_rooms, name="search-rooms"),
 ]
 
--- booking/views.py
+++ booking/views.py
@@ -4,3 +4,8 @@ from rest_framework.response import Response
 @api_view(['GET']) 
 def hello_word(request):
     return Response({"message":"hello"})
+
+@api_view(['GET'])
+def search_rooms(request, start_date, end_date, guests):
+    
+    return Response({"response":""} )
