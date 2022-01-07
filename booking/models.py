00dc6ad5c258e51bc938912507bdb19e01b8ed9c
Eddy <eddypv0@gmail.com>
2022-01-06 23:11:26 -0400

    Include in the Model Room the field facilities

--- booking/models.py
+++ booking/models.py
@@ -5,17 +5,20 @@ class RoomService(models.Model):
   cod_service = models.CharField(max_length=20, primary_key=True)
   service = models.CharField(max_length=100)
 
+class RoomFacility(models.Model):
+    cod_facility = models.CharField(max_length=20, primary_key=True)
+    facility = models.CharField(max_length=100)
+
 class Room(models.Model):
   title = models.CharField(max_length=200)
   description = models.CharField(max_length=1000)
   guests = models.PositiveSmallIntegerField() # cantidad huspedes
   beds = models.PositiveSmallIntegerField() #cantidad camas
   toilets = models.PositiveSmallIntegerField() #cantidad camas
+  facilities = models.ManyToManyField(RoomFacility) 
 
-class RoomFacility(models.Model):
-    cod_facility = models.CharField(max_length=20, primary_key=True)
-    facility = models.CharField(max_length=100)
     
+
 
 
 
