35c6bcce5c4d97395adfff4ba1ee93ab210690d2
Eddy <eddypv0@gmail.com>
2022-01-06 22:33:18 -0400

    Create Model RoomService

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class RoomService(models.Model):
+  cod_service = models.CharField(max_length=20,primary_key=True)
+  service = models.CharField(max_length=100)
